4e4cd6adbef15887964a02ba7b5eca339989fd35
Dominik Jelicic <dominik246@gmail.com>
2020-05-03 17:18:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DiscordBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new ProgramAsync().MainAsync().GetAwaiter().GetResult();
+    }
+}
---
+++ ProgramAsync.cs
@@ -0,0 +1,40 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class ProgramAsync
+    {
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            try
+            {
+                await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
+                await _client.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                await Log(new LogMessage(LogSeverity.Critical, ex.Source, ex.Message));
+            }
+            finally
+            {
+                await Task.Delay(-1);
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
